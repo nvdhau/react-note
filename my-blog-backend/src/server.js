31f5a917e7922566258c57214b543d7f71e8874b
nvdhau <nvdhau@gmail.com>
2020-03-15 16:29:51 -0700

    refactor access mongoDB method

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -21,44 +21,55 @@ import {MongoClient} from 'mongodb'
 const app = express();
 app.use(bodyParser.json());
 
-
-app.get('/api/articles/:name', async (req, res) => {
-  
+//refactor mongoDB
+const withDB = async (operations, res) => {
   try{
-    const articleName = req.params.name;
-
     //connect to MongoServer
     const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true});
     const db = client.db('my-blog');//get db
 
-    const articleInfo = await db.collection('articles').findOne({name: articleName})
+    await operations(db);
 
-    res.status(200).json(articleInfo);
   }catch(err){
     res.status(500).json({message: 'Error connecting to db', err});
   }finally{
     client.close();
   }
+}
+
+app.get('/api/articles/:name', async (req, res) => {
+  
+  withDB(async (db) => {
+    const articleName = req.params.name;
+    const articleInfo = await db.collection('articles').findOne({name: articleName})
+    res.status(200).json(articleInfo);
+  }, res);//PASS res object
+
 })
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
   const articleName = req.params.name;
   const {username, text} = req.body;
 
-  articlesInfo[articleName].comments.push({username, text})
+  withDB(async (db) => {
 
-  res.status(200).send(articlesInfo[articleName])
+    const articleInfo = await db.collection.findOne({name: articleName})
+    await db.collection('articles').updateOne({name: articleName}, 
+      {'$set':{
+        comments: articleInfo.comments.concat({username, text})
+      }
+    });
+    const updatedArticleInfo = await db.collection.findOne({name: articleName})
+
+    res.status(200).json(updatedArticleInfo);
+  }, res)//PASS res object
 })
 
 app.post('/api/articles/:name/upvote', async (req, res) => {
   
-  try{
+  withDB(async (db) => {
     const articleName = req.params.name;
-  
-    //connect to MongoServer
-    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true});
-    const db = client.db('my-blog');//get db
-
+    
     const articleInfo = await db.collection.findOne({name: articleName})
     await db.collection('articles').updateOne({name: articleName}, 
       {'$set':{
@@ -68,12 +79,7 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
     const updatedArticleInfo = await db.collection.findOne({name: articleName})
 
     res.status(200).json(updatedArticleInfo);
-
-  }catch(err){
-    res.status(500).json({message: 'Error connecting db', err});
-  }finally{
-    client.close();
-  }
+  }, res)//PASS res object
 })
 
 app.listen(8000, () => console.log('Listenning on port 8000'))
