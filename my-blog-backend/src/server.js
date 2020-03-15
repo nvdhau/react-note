42ede63805081a23afdd0c5d9a5f94a536d94424
nvdhau <nvdhau@gmail.com>
2020-03-15 15:02:31 -0700

    added simple express backend

---
+++ my-blog-backend/src/server.js
@@ -0,0 +1,8 @@
+//support ES6
+import express from 'express'
+
+const app = express();
+
+app.get('/hello', (req, res) => res.send('Hello!'));
+
+app.listen(8000, () => console.log('Listenning on port 8000'))
