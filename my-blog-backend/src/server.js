a428b2944a0f5421c767ec150d7b194da35f90fa
nvdhau <nvdhau@gmail.com>
2020-03-15 15:26:26 -0700

    added body-parser and and nodemon

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,8 +1,28 @@
 //support ES6
 import express from 'express'
+import bodyParser from 'body-parser'
+
+//mock-up db
+const articlesInfo = {
+  'learn-react': {
+    upvotes: 0,
+  },
+  'learn-node':{
+    upvotes: 0
+  },
+  'my-thoughts-on-resumes':{
+    upvotes: 0
+  }
+}
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/hello', (req, res) => res.send('Hello!'));
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
+  articlesInfo[articleName].upvotes++;
+
+  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes}`)
+})
 
 app.listen(8000, () => console.log('Listenning on port 8000'))
