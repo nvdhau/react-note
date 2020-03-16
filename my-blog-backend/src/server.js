953728aabddd77e4512375b66d102c61321db9bd
nvdhau <nvdhau@gmail.com>
2020-03-15 18:24:53 -0700

    fixed back-end errors in update MongoDB data

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -53,13 +53,13 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
 
   withDB(async (db) => {
 
-    const articleInfo = await db.collection.findOne({name: articleName})
+    const articleInfo = await db.collection('articles').findOne({name: articleName})
     await db.collection('articles').updateOne({name: articleName}, 
       {'$set':{
         comments: articleInfo.comments.concat({username, text})
       }
     });
-    const updatedArticleInfo = await db.collection.findOne({name: articleName})
+    const updatedArticleInfo = await db.collection('articles').findOne({name: articleName})
 
     res.status(200).json(updatedArticleInfo);
   }, res)//PASS res object
@@ -70,13 +70,13 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
   withDB(async (db) => {
     const articleName = req.params.name;
     
-    const articleInfo = await db.collection.findOne({name: articleName})
+    const articleInfo = await db.collection('articles').findOne({name: articleName})
     await db.collection('articles').updateOne({name: articleName}, 
       {'$set':{
         upvotes: articleInfo.upvotes + 1
       }
     });
-    const updatedArticleInfo = await db.collection.findOne({name: articleName})
+    const updatedArticleInfo = await db.collection('articles').findOne({name: articleName})
 
     res.status(200).json(updatedArticleInfo);
   }, res)//PASS res object
