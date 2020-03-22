461a48d241aac5c3355c207b6fb32ce3dd7fce0b
nvdhau <nvdhau@gmail.com>
2020-03-21 18:17:59 -0700

    added create, display player list and player info

--- soccer-app/frontend/src/Components/App.js
+++ soccer-app/frontend/src/Components/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import './App.css'
 import PlayerList from './Player/PlayerList'
 import PlayerSingle from './Player/PlayerSingle'
 import PlayerForm from './Player/PlayerForm'
 
-function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col s12">Menu</div>
-      </div>
+class App extends React.Component {
+  constructor(props){
+    super(props)
 
-      <div className="row">
-        <div className="col s3"><PlayerList /></div>
-        <div className="col s9"><PlayerSingle /></div>
-      </div>
+    this.state = {
+      players: [],
+      currentPlayer: {},
+    }
 
-      <div className="row">
-        <div className="col s12"><PlayerForm /></div>
+    //bind a func
+    this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this);
+  }
+
+  componentDidMount(){
+    const url = 'http://localhost:8000/players';
+
+    axios.get(url)
+      .then((Response) => {
+        this.setState({
+          players: Response.data
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  //create func
+  updateCurrentPlayer(item){
+    this.setState({
+      currentPlayer: item,
+    })
+  }
+  
+  render(){
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <nav>
+            <div className="nav-wrapper blue darken-1">
+              <a href="/" className="brand-logo">Soccer Management</a>
+            </div>
+          </nav>
+        </div>
+
+        <div className="row">
+          <div className="col s3"><PlayerList players={this.state.players}
+            updateCurrentPlayer={this.updateCurrentPlayer}
+          /></div>
+          <div className="col s9"><PlayerSingle player={this.state.currentPlayer} /></div>
+        </div>
+
+        <div className="row">
+          <div className="col s12"><PlayerForm /></div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- soccer-app/frontend/src/Components/Player/PlayerSingle.js
+++ soccer-app/frontend/src/Components/Player/PlayerSingle.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const PlayerSingle = () => {
-  return ( <div>
-    PlayerSingle
-  </div> );
+const PlayerSingle = ({player}) => {
+
+  return ( 
+    <div className="row">
+      <div className="col s12">
+        <div className="card">
+          <div className="card-image">
+            <img src="soccer.jpg" alt="soccer"/>
+            <span className="card-title">{player.firstName} {player.lastName}</span>
+          </div>
+          <div className="card-content">
+            <p>Phone: {player.phone} - Email: {player.email}</p>
+            <p>Strength: {player.strength} - Endurance: {player.endurance}</p>
+          </div>
+          <div className="card-action">
+            Team: {player.team}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
  
 export default PlayerSingle;
